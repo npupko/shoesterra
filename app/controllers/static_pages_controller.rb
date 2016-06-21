d76dddd474f702d98e463552c243232b04affee3
Random4405 <random4405@gmail.com>
2016-06-22 00:12:48 +0300

    Full search started

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,13 @@ class StaticPagesController < ApplicationController
   def catalog
     @query = Product.search do
       fulltext params[:search]
+      facet(:sex)
+      facet(:price)
+      facet(:min_size)
+      facet(:max_size)
+      with(:sex, params[:sex]) if params[:sex].present?
+      with(:price, params[:price]) if params[:price].present?
+      with(:min_size, greater_than(params[:min_size])) if params[:min_size].present?
     end
     @products = @query.results
   end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,14 @@ class Product < ActiveRecord::Base
   validates_attachment_content_type :main_image, content_type: /\Aimage\/.*\Z/
   searchable do
     text :brand
+    float :price
+    integer :min_size
+    integer :max_size
+    string :season
+    string :sex
+    string :top_matereal
+    string :pad_matereal
+    string :sole_matereal
+    string :shoe_type
 end
 end
